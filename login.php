f8bf33a8db5a6eac55416c3481216771bfc57ed4
soukrat <soukrat.abdelghani@gmail.com>
2019-01-21 22:32:28 +0100

    first commit

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</head>
+ 
+<body style="background-image: url('img/back.png');">
+    <div class="container">
+            
+         <div class="content">
+          <div class="row">
+
+            <div class="col-md-4 col-sm-12 align-center offset-md-4 shadow p-3 mb-5 bg-white rounded" style="background-color: #fff; margin-top: 12%;padding: 3%">
+               
+                            
+                            <form class="" method="POST" action="connect.php">
+                                
+
+                                <div class="block block-themed block-rounded block-shadow">
+                                    <div class="block-header bg-gd-dusk">
+                                        <h3 class="block-title">Monuments Catalog Login</h3>
+                                  
+                                    </div>
+                                
+                                    <div class="block-content">
+                                        <div class="form-group row">
+                                            <div class="col-md-12">
+                                              
+                                            </div>
+                                            <div class="col-12">
+                                                <label for="login-username">E-mail</label>
+                                                <input type="text" class="form-control" id="email" name="email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <label for="login-password">Password</label>
+                                                <input type="password" class="form-control" id="password" name="password" >
+                                            </div>
+                                        </div>
+                                        <div class="form-group row mb-0">
+                                        
+                                            <div class="col-sm-6 text-sm-right push">
+                                                <button type="submit" class="btn btn-primary">
+                                                    <i class="si si-login mr-10"></i> Se connecter
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            
+
+    </div> <!-- /container -->
+  </body>
+</html>
